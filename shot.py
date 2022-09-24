de408612ec3052fd1a2735c3fca17b1628a2fe67
jabuchae <jabuchae@gmail.com>
2022-09-24 20:32:05 -0300

    Faster by quadrant

--- shot.py
+++ shot.py
@@ -31,37 +31,113 @@ def solution(room, me, enemy, max_distance):
 
         return [new_room_x, new_room_y]
 
-    def hits_me_first(enemy_pos, room_num_x, room_num_y):
+    def hits_me_first(room_num_x, room_num_y):
         if room_num_x == 0 and room_num_y == 0:
             return False
         
         if room_num_x == 0:
-            return enemy_pos[0] == me[0]
+            return enemy[0] == me[0]
         
         if room_num_y == 0:
-            return enemy_pos[1] == me[1]
+            return enemy[1] == me[1]
 
         if room_num_y == room_num_x:
             return slope([room_x, room_y], me) == slope(enemy, me)
 
         return False
 
+    def hits(room_num_x, room_num_y):
+        enemy_pos = translate(enemy, room_num_x, room_num_y)
+        if hits_me_first(room_num_x, room_num_y):
+            return False
+        return not out_of_range(enemy_pos)
+
     max_rooms_x = int(max_distance/room_x)+1
     max_rooms_y = int(max_distance/room_y)+1
 
     shots = 0
     
-    room_num_x = -max_rooms_x
-    max_room_y = max_rooms_y
+    v_aligned = me[0] == enemy[0]
+    h_aligned = me[1] == enemy[1]
+    upper_right_aligned = slope([room_x, room_y], enemy) == slope([room_x, room_y], me)
+    upper_left_aligned = slope([0, room_y], enemy) == slope([0, room_y], me)
+    lower_right_aligned = slope([room_x, 0], enemy) == slope([room_x, 0], me)
+    lower_left_aligned = slope([0, 0], enemy) == slope([0, 0], me)
+
+    h_fix = 0 if h_aligned else 1
+    upper_right_fix = 1 if upper_right_aligned else 0
+    upper_left_fix = 1 if upper_left_aligned else 0
+    lower_right_fix = 1 if lower_right_aligned else 0
+    lower_left_fix = 1 if lower_left_aligned else 0
+
+    room_num_x = 1
     while room_num_x <= max_rooms_x:
-        room_num_y = max_room_y
-        while room_num_y >= -max_room_y:
-            enemy_pos = translate(enemy, room_num_x, room_num_y)
-            if not out_of_range(enemy_pos) and not hits_me_first(enemy_pos, room_num_x, room_num_y):
-                shots += 1
+        room_num_y = max_rooms_y
+        while room_num_y >= 0:
+            if hits(room_num_x, room_num_y):
+                shots += room_num_y + h_fix
+                if room_num_y >= room_num_x:
+                    shots -= upper_right_fix
+                break
             room_num_y -= 1
+
+        room_num_y = -max_rooms_y
+        while room_num_y < 0:
+            if hits(room_num_x, room_num_y):
+                shots += -room_num_y
+                if -room_num_y >= room_num_x:
+                    shots -= lower_right_fix
+                break
+            room_num_y += 1
+
         room_num_x += 1
     
+    room_num_x = -1
+    while room_num_x >= -max_rooms_x:
+        room_num_y = max_rooms_y
+        while room_num_y >= 0:
+            if hits(room_num_x, room_num_y):
+                shots += room_num_y + h_fix
+                if room_num_y >= -room_num_x:
+                    shots -= upper_left_fix
+                break
+            room_num_y -= 1
+
+        room_num_y = -max_rooms_y
+        while room_num_y < 0:
+            if hits(room_num_x, room_num_y):
+                shots += -room_num_y
+                if -room_num_y >= -room_num_x:
+                    shots -= lower_left_fix
+                break
+            room_num_y += 1
+
+        room_num_x -= 1
+
+    if not v_aligned:
+        room_num_x = 0
+        room_num_y = max_rooms_y
+        while room_num_y >= 0:
+            if hits(room_num_x, room_num_y):
+                shots += room_num_y + h_fix
+                if room_num_y >= room_num_x:
+                    shots -= upper_right_fix
+                break
+            room_num_y -= 1
+
+        room_num_y = -max_rooms_y
+        while room_num_y < 0:
+            if hits(room_num_x, room_num_y):
+                shots += -room_num_y
+                if -room_num_y >= room_num_x:
+                    shots -= lower_right_fix
+                break
+            room_num_y += 1
+
+    if h_aligned and hits(0,0):
+        shots += 1
+    if v_aligned and hits(0,0):
+        shots += 1
     return shots
 
 if __name__=='__main__':
